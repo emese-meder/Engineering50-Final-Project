8ec86fbf1a13ec9505652076ffe46b5f93723875
Gwilym Turner <Freebird112>
2020-02-27 15:26:04 +0000

    Refactored information navigation

---
+++ ProjectBlackbeard/src/test/java/com/sparta/eng50/pageModels/AboutUsPage.java
@@ -0,0 +1,9 @@
+package com.sparta.eng50.pageModels;
+
+import org.openqa.selenium.WebDriver;
+
+public class AboutUsPage extends AbstractPage{
+    public AboutUsPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+}
