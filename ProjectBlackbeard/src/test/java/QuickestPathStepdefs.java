6b0b6039ef45928f8daa4a94245a6789c1711b62
emese-meder <58849918+emese-meder@users.noreply.github.com>
2020-02-27 15:15:39 +0000

    changed to english

--- ProjectBlackbeard/src/test/java/QuickestPathStepdefs.java
+++ ProjectBlackbeard/src/test/java/QuickestPathStepdefs.java
@@ -1,4 +1,8 @@
 import com.sparta.eng50.pageModels.*;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import io.cucumber.java.en_pirate.Aye;
 import io.cucumber.java.en_pirate.Blimey;
 import io.cucumber.java.en_pirate.Gangway;
@@ -21,23 +25,23 @@ public class QuickestPathStepdefs {
     PaymentConfirmationPage paymentConfirmationPage;
     OrderConfirmationPage orderConfirmationPage;
 
-    @Gangway("I am on the home page")
+    @Given("I am on the home page")
     public void iAmOnTheHomePage() {
         homePage.navigateToHomePage();
     }
 
-    @Blimey("I hover over a popular item and click add to basket")
+    @When("I hover over a popular item and click add to basket")
     public void iHoverOverAPopularItemAndClickAddToBasket() {
         homePage.addItemToCart();
     }
 
-    @Letgoandhaul("I should be notified that the item has been added to my cart")
+    @Then("I should be notified that the item has been added to my cart")
     public void iShouldBeNotifiedThatTheItemHasBeenAddedToMyCart() {
         assertEquals("Product successfully added to your shopping cart", homePage.getCartNotification());
         webDriver.quit();
     }
 
-    @Blimey("I click proceed to checkout")
+    @When("I click proceed to checkout")
     public void iClickProceedToCheckout() {
         try {
             Thread.sleep(2000);
@@ -47,24 +51,24 @@ public class QuickestPathStepdefs {
         cartPage = homePage.clickProceedToCheckout();
     }
 
-    @Letgoandhaul("I should go to the checkout with the item in the cart")
+    @Then("I should go to the checkout with the item in the cart")
     public void iShouldGoToTheCheckoutWithTheItemInTheCart() {
         assertEquals("Faded Short Sleeve T-shirts", cartPage.getProductText());
         webDriver.quit();
     }
 
-    @Blimey("I click on the proceed to checkout button")
+    @When("I click on the proceed to checkout button")
     public void iClickOnTheProceedToCheckoutButton() {
         signInPage = cartPage.clickProceedToCheckout();
     }
 
-    @Letgoandhaul("I am transported to the checkout sign in page")
+    @Then("I am transported to the checkout sign in page")
     public void iAmTransportedToTheCheckoutSignInPage() {
         assertEquals(signInPage.getBreadcrumbText(),"Authentication");
         webDriver.quit();
     }
 
-    @Blimey("I enter an email address and click create an account")
+    @When("I enter an email address and click create an account")
     public void iEnterAnEmailAddressAndClickCreateAnAccount() {
         try {
             Thread.sleep(2000);
@@ -74,7 +78,7 @@ public class QuickestPathStepdefs {
         accountCreationPage = signInPage.enterEmailAddress(EmailRandomiser.getRandomEmailAddress());
     }
 
-    @Blimey("I enter in valid details for account creation")
+    @When("I enter in valid details for account creation")
     public void iEnterInValidDetailsForAccountCreation() {
         try {
             Thread.sleep(2000);
@@ -84,25 +88,25 @@ public class QuickestPathStepdefs {
         addressSelectorPage = accountCreationPage.enterSignUpForm();
     }
 
-    @Letgoandhaul("I should have an account made")
+    @Then("I should have an account made")
     public void iShouldHaveAnAccountMade() {
         assertEquals("http://automationpractice.com/index.php?controller=order&step=1&multi-shipping=0", webDriver.getCurrentUrl());
         webDriver.quit();
     }
 
-    @Blimey("I click on the proceed to checkout button on the address page")
+    @When("I click on the proceed to checkout button on the address page")
     public void iClickOnTheProceedToCheckoutButtonOnTheAddressPage() {
         shippingPage = addressSelectorPage.clickProceedToCheckout();
     }
 
-    @Letgoandhaul("I should be on the shipping page")
+    @Then("I should be on the shipping page")
     public void iShouldBeOnTheShippingPage() {
         assertEquals("http://automationpractice.com/index.php?controller=order", webDriver.getCurrentUrl());
         webDriver.quit();
     }
 
 
-    @Blimey("I agree to the terms and conditions")
+    @When("I agree to the terms and conditions")
     public void iAgreeToTheTermsAndConditions() {
         try {
             Thread.sleep(1500);
@@ -112,34 +116,34 @@ public class QuickestPathStepdefs {
         shippingPage.clickOnTermsAndConditions();
     }
 
-    @Aye("I then click the proceed to checkout button on the shipping page")
+    @And("I then click the proceed to checkout button on the shipping page")
     public void iThenClickTheProceedToCheckoutButtonOnTheShippingPage() {
         paymentPage = shippingPage.proceedButton();
     }
 
-    @Letgoandhaul("I should be on the payment page")
+    @Then("I should be on the payment page")
     public void iShouldBeOnThePaymentPage() {
         assertEquals("http://automationpractice.com/index.php?controller=order&multi-shipping=", webDriver.getCurrentUrl());
         webDriver.quit();
     }
 
-    @Blimey("I choose to pay by bank wire")
+    @When("I choose to pay by bank wire")
     public void iChooseToPayByBankWire() {
         paymentConfirmationPage = paymentPage.clickPayByBankWire();
     }
 
-    @Letgoandhaul("I should be on the payment confirmation page")
+    @Then("I should be on the payment confirmation page")
     public void iShouldBeOnThePaymentConfirmationPage() {
         assertEquals("http://automationpractice.com/index.php?fc=module&module=bankwire&controller=payment", webDriver.getCurrentUrl());
         webDriver.quit();
     }
 
-    @Blimey("I click I confirm my order")
+    @When("I click I confirm my order")
     public void iClickIConfirmMyOrder() {
         orderConfirmationPage = paymentConfirmationPage.clickOnConfirmButton();
     }
 
-    @Letgoandhaul("I will be able to view the order details page")
+    @Then("I will be able to view the order details page")
     public void iWillBeAbleToViewTheOrderDetailsPage() {
         assertEquals("Order confirmation", orderConfirmationPage.getBreadcrumbText());
         webDriver.quit();
